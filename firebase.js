b9d51f89cd9298669360535bdf813d1f633cbded
aaron lopez <aaron_lopez222@hotmail.com>
2025-11-26 20:31:48 -0600

    deploy commit message

--- firebase.js
+++ firebase.js
@@ -1,17 +1,8 @@
 const admin = require("firebase-admin");
-const config = require("./config");
-
-if (!config.firebase.privateKey) {
-  console.error("❌ ERROR: FIREBASE_PRIVATE_KEY no fue cargada.");
-  process.exit(1);
-}
+const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    project_id: config.firebase.projectId,
-    client_email: config.firebase.clientEmail,
-    private_key: config.firebase.privateKey,
-  }),
+    credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
