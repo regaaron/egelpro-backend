1b7df98651cc210522c734442ba969bbde954316
aaron lopez <aaron_lopez222@hotmail.com>
2025-11-26 19:30:20 -0600

    deploy

---
+++ config.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+module.exports = {
+  port: process.env.PORT || 3000,
+
+  db: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  },
+
+  openai: {
+    key: process.env.OPENAI_API_KEY,
+    model: process.env.OPENAI_MODEL || "gpt-4o-mini",
+    enabled: process.env.OPENAI_ENABLED === "true"
+  }
+};
